e371d07a948f74854a9980160f0f8eaf40fc1e4b
jrr <john.ruble@atomicobject.com>
2018-06-08 01:16:09 -0400

    net471, timer trigger, package updates

--- WebJob-Traditional/Functions.cs
+++ WebJob-Traditional/Functions.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
 
 namespace WebJob_Traditional
 {
@@ -12,9 +14,9 @@ namespace WebJob_Traditional
     {
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        public static void SayHello([TimerTrigger("0 */1 * * * *")] TimerInfo timerInfo, TraceWriter log)
         {
-            log.WriteLine(message);
+            log.Info("hello from traditional C# webjob.");
         }
     }
 }
