8a99fe4aaaea4854d354fbafc15e074943cb6de6
jrr <john.ruble@atomicobject.com>
2018-06-08 01:35:20 -0400

    dotnet new console

---
+++ WebJob-dotnet-cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebJob_dotnet_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
